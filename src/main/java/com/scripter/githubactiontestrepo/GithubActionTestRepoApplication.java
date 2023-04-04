00f085b9c78ba703b5402281511e27fbea72095b
ScriptKiddie47 <ritambala@live>
2023-04-04 11:44:38 +0530

    first_commit

---
+++ src/main/java/com/scripter/githubactiontestrepo/GithubActionTestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.scripter.githubactiontestrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionTestRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionTestRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scripter/githubactiontestrepo/GithubActionTestRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scripter.githubactiontestrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionTestRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
